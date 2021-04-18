c72454ae3f35c3cf195e92eb619226898e4b636a
Mike Bourgeous <mike@mikebourgeous.com>
2021-04-18 13:47:43 -0600

    Added very rudimentary tests for .syntax and .highlight.

--- spec/lib/mb/util_spec.rb
+++ spec/lib/mb/util_spec.rb
@@ -1,4 +1,16 @@
 RSpec.describe(MB::Util) do
+  describe '.highlight' do
+    it 'includes some ANSI colors when given a Hash' do
+      expect(MB::Util.highlight({a: 1})).to match(/\e\[[^a-z]*m/)
+    end
+  end
+
+  describe '.syntax' do
+    it 'includes some ANSI colors when given some code' do
+      expect(MB::Util.syntax("def x; {a: 1}; end")).to match(/\e\[[^a-z]*m/)
+    end
+  end
+
   describe '.remove_ansi' do
     it 'can remove color codes from text' do
       input = "\e[1mBold\e[0m \e[34mBlue\e[0m \e[1;32mBright Green\e[0m \e[48;2;100;50;50mRust Background\e[0m"
