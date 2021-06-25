e083d47981e5930e9abb869d5a85d8fc2f6c620d
Mike Bourgeous <mike@mikebourgeous.com>
2021-06-25 11:39:34 -0600

    Add a test for width and height when IO.console returns nil.

--- spec/lib/mb/util_spec.rb
+++ spec/lib/mb/util_spec.rb
@@ -23,4 +23,18 @@ RSpec.describe(MB::Util) do
       expect(MB::Util.remove_ansi(input)).to eq('Hithere')
     end
   end
+
+  [:width, :height].each do |m|
+    describe ".#{m}" do
+      it 'returns an integer value' do
+        expect(MB::Util.send(m)).to be_a(Integer)
+      end
+
+      it 'works even if IO.console returns nil' do
+        expect(IO).to receive(:console).at_least(2).times.and_return(nil)
+        expect(MB::Util.send(m)).to be_a(Integer)
+        expect(IO.console).to be_nil
+      end
+    end
+  end
 end
